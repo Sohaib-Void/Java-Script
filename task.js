e6eecc2cc301d7359c16d8222c0d5694ec1d93a4
Sohaib-Void <sohaibkhan.aiub@gmail.com>
2025-07-28 00:59:18 +0600

    update task

---
+++ task.js
@@ -0,0 +1,24 @@
+//Problem 1//
+var apple = 300;
+var orange = 400;
+var money = 1000;
+
+var moneyBack = money - (apple + orange);
+console.log(moneyBack);
+
+
+//Problem -2 //
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bangl = 99.50;
+
+var totalMarks = (math + bio + chem + phy + bangl) / 5;
+console.log(totalMarks.toFixed(2) );
+
+
+//problem-3//
+var a = 119;
+var div = a % 5;
+console.log(div);
